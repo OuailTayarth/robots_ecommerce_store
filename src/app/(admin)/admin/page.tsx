d4466dedf09b857c0517889926f1a9af2bd58242
Tayarthouail <tayarthouail@gmail.com>
2025-07-30 09:03:32 +0200

    delete admin and modify data

--- src/app/(admin)/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-async function AdminDashboard() {
-  redirect("/admin/dashboard");
-}
-
-export default AdminDashboard;
---
+++ src/lib/stripe/index.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env.mjs";
+import Stripe from "stripe";
+
+export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+  appInfo: {
+    name: "RobotForge",
+    version: "0.1.0",
+  },
+});
+
