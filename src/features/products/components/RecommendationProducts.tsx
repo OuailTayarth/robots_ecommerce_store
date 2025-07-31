86fbe3c250814d30a84211bc6044440aa81f413d
Tayarthouail <tayarthouail@gmail.com>
2025-07-31 22:20:01 +0200

    add wishlistQuery

--- src/features/products/components/RecommendationProducts.tsx
+++ src/features/products/components/RecommendationProducts.tsx
@@ -4,13 +4,15 @@ import { useQuery } from "@urql/next";
 import React from "react";
 import Header from "@/components/layouts/Header";
 import { ProductCard } from "@/features/products";
+import { useAuth } from "@/providers/AuthProvider";
+
 import ProductCardSkeleton from "./RecommendationProductsSkeleton";
 
 export type RecommendationProductsProps =
   React.HTMLAttributes<HTMLDivElement> & {};
 
-const RecomendationProductsQuery = gql(/* GraphQL */ `
-  query RecomendationProductsQuery($first: Int!) {
+const RecommendationProductsQuery = gql(/* GraphQL */ `
+  query RecommendationProductsQuery($first: Int!) {
     recommendations: productsCollection(first: $first) {
       edges {
         node {
@@ -22,13 +24,37 @@ const RecomendationProductsQuery = gql(/* GraphQL */ `
   }
 `);
 
+const WishlistEmptyQuery = gql(/* GraphQL */ `
+  query WishlistEmptyQuery($userId: UUID!) {
+    wishlist: wishlistCollection(
+      filter: { user_id: { eq: $userId } }
+      first: 1
+    ) {                     
+      edges {               
+        node { product_id }
+      }
+    }
+  }
+`);
+
 function RecommendationProducts({}: RecommendationProductsProps) {
+  const {user} = useAuth();
+  const userId = user?.id
   const [{ data, fetching, error }, refetch] = useQuery({
-    query: RecomendationProductsQuery,
+    query: RecommendationProductsQuery,
     variables: {
       first: 4,
     },
   });
+
+  const [{ data: wish }] = useQuery({
+    query: WishlistEmptyQuery,
+    variables: { userId },
+    pause: !userId,
+  });
+
+  const isWishListEmpty = !wish || wish?.wishlist?.edges.length === 0;
+
 
   if (fetching)
     return (
@@ -44,6 +70,9 @@ function RecommendationProducts({}: RecommendationProductsProps) {
   if (!data || error) return <></>;
 
   return (
+    <>
+    isWishList
+
     <Header heading={`Popular Picks This Week!`}>
       <div className="container grid grid-cols-2 lg:grid-cols-4 gap-x-8 ">
         {data.recommendations &&
@@ -52,6 +81,8 @@ function RecommendationProducts({}: RecommendationProductsProps) {
           ))}
       </div>
     </Header>
+    </>
+    
   );
 }
 
