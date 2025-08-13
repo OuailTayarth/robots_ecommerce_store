425efabcf8c0cbfdf46e18c51fce886d4ca9571b
OuailT <tayarthouail@gmail.com>
2025-08-13 10:47:21 +0200

    fix: buy now button and build

--- src/features/products/components/BuyNowButton.tsx
+++ src/features/products/components/BuyNowButton.tsx
@@ -1,13 +1,19 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 type BuyNowButtonProps = {
   productId: string;
 };
 
 function BuyNowButton({ productId }: BuyNowButtonProps) {
-  return <Button type="submit">Buy Now</Button>;
+  // to fix later
+  return (
+    <Link href={"/cart"}>
+      <Button type="submit">Buy Now</Button>
+    </Link>
+  );
 }
 
 export default BuyNowButton;
